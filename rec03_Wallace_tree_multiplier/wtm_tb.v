77b97449cf6427283c8f4c05830f944367951faa
JinchengHeRyan <delteforce@outlook.com>
2022-09-16 02:22:00 -0400

    Update testbench and passed all the tests

--- rec03_Wallace_tree_multiplier/wtm_tb.v
+++ rec03_Wallace_tree_multiplier/wtm_tb.v
@@ -22,29 +22,60 @@ module wtm_tb();
     successes = 0;
 
     @(negedge clock);
-    assign in1 = 5'b10010;
-    assign in2 = 5'b00010;
+    assign in1 = 5'd3;
+    assign in2 = 5'd8;
 
     @(negedge clock);
-    if (result != 10'b0000100100 || cout != 1'b0) begin
-      $display("test 1 failed; expected: %h, actual: %h", 10'b0000100100, result);
+    if (result != 10'd24 || cout != 1'b0) begin
+      $display("test 1 failed; expected: %h, actual: %h", 10'd24, result);
+      errors = errors + 1;
+    end else begin
+      successes = successes + 1;
+    end
+
+
+    @(negedge clock);
+    assign in1 = 5'd20;
+    assign in2 = 5'd0;
+
+    @(negedge clock);
+    if (result != 10'd0 || cout != 1'b0) begin
+      $display("test 2 failed; expected: %h, actual: %h", 10'd0, result);
+      errors = errors + 1;
+    end else begin
+      successes = successes + 1;
+    end
+
+
+    @(negedge clock);
+    assign in1 = 5'd25;
+    assign in2 = 5'd16;
+
+    @(negedge clock);
+    if (result != 10'd400 || cout != 1'b0) begin
+      $display("test 3 failed; expected: %h, actual: %h", 10'd400, result);
       errors = errors + 1;
     end else begin
       successes = successes + 1;
     end
 
     @(negedge clock);
-    assign in1 = 5'b10010;
-    assign in2 = 5'b00011;
+    assign in1 = 5'd31;
+    assign in2 = 5'd1;
 
     @(negedge clock);
-    if (result != 10'b0000110110 || cout != 1'b0) begin
-      $display("test 2 failed; expected: %h, actual: %h", 10'b0000110110, result);
+    if (result != 10'd31 || cout != 1'b0) begin
+      $display("test 4 failed; expected: %h, actual: %h", 10'd31, result);
       errors = errors + 1;
     end else begin
       successes = successes + 1;
     end
 
+
+    @(negedge clock);
+    if (errors == 0) begin
+      $display("Passed all %d tests", successes);
+    end
 
     $stop;
   end
