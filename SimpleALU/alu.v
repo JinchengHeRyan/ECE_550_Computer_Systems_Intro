6725bc7a7511590c0f1b43f1c93df0b23d308113
JinchengHeRyan <delteforce@outlook.com>
2022-09-17 03:22:31 -0400

    Add wire add_or_sub controled by Opcode

--- SimpleALU/alu.v
+++ SimpleALU/alu.v
@@ -11,8 +11,11 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
    wire [31:0]add_sub_output;
    wire add_circuit_cout;
    wire A_B_xor;
+
+   wire add_or_sub;  // controled by Opcode
+   assign add_or_sub = ctrl_ALUopcode[0] ? 1'b1 : 1'b0;
    
-   add_circuit add_c(add_sub_output, overflow, data_operandA, data_operandB, ctrl_ALUopcode[0]);
+   add_circuit add_c(add_sub_output, overflow, data_operandA, data_operandB, add_or_sub);
 
    chooseOp choose_op(add_sub_output, add_sub_output, ctrl_ALUopcode, data_result);
 
