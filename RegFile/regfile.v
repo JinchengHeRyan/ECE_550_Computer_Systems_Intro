71b2d689d7a11450e09b16748632d0b34eee1de4
JinchengHeRyan <delteforce@outlook.com>
2022-10-03 01:39:53 -0400

    Update issue of reg 0 should always be zero

--- RegFile/regfile.v
+++ RegFile/regfile.v
@@ -19,10 +19,14 @@ module regfile (
    decode5to32 decode_writeReg(ctrl_writeReg, writeReg_onehot);
    decode5to32 decode_regA(ctrl_readRegA, regA_onehot);
    decode5to32 decode_regB(ctrl_readRegB, regB_onehot);
+
+   // reg 0
+   wire [31:0] reg_output_0;
+   onereg reg_0(data_writeReg, reg_output_0, clock, 1'b1, ctrl_writeEnable & writeReg_onehot[0]);
    
    genvar i;
    generate
-     for (i = 0; i <= 31; i = i + 1) begin:regs_build
+     for (i = 1; i <= 31; i = i + 1) begin:regs_build
        wire [31:0] reg_output_i;
        onereg reg_i(
          .input_data  (data_writeReg), 
@@ -36,8 +40,11 @@ module regfile (
    endgenerate
 
 
+   assign data_readRegA = regA_onehot[0] ? reg_output_0 : 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+   assign data_readRegB = regB_onehot[0] ? reg_output_0 : 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
+
    generate
-     for (i = 0; i <= 31; i = i + 1) begin:read_A_block
+     for (i = 1; i <= 31; i = i + 1) begin:read_A_block
        assign data_readRegA = regA_onehot[i] ? regs_build[i].reg_output_i : 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
        assign data_readRegB = regB_onehot[i] ? regs_build[i].reg_output_i : 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
 
