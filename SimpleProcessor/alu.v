63060b3d6fed33e9c78321855276372953ff9d45
JinchengHeRyan <delteforce@outlook.com>
2022-11-11 04:07:02 -0500

    Update ALU with overflow only in add and sub

--- SimpleProcessor/alu.v
+++ SimpleProcessor/alu.v
@@ -9,6 +9,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode,
     wire signed[31:0] inner_A, inner_B;
     reg signed[31:0] inner_result;
     reg inner_cout;
+    reg overflow_result;
 
     assign inner_A = data_operandA;
     assign inner_B = data_operandB;
@@ -16,15 +17,23 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode,
 
     assign isNotEqual = inner_A != inner_B;
     assign isLessThan = inner_A < inner_B;
-    assign overflow = inner_cout != inner_result[31];
+    // assign overflow = inner_cout != inner_result[31];
+    assign overflow = overflow_result;
 
     always @(ctrl_ALUopcode or inner_A or inner_B or ctrl_shiftamt)
         begin
             // Default state for other ctrl_ALUopcode states
-            {inner_cout, inner_result} = inner_A+inner_B;
+            // {inner_cout, inner_result} = inner_A+inner_B;
+            overflow_result = 0;
             case (ctrl_ALUopcode)
-                0: {inner_cout, inner_result} = inner_A+inner_B;  // ADD
-                1: {inner_cout, inner_result} = inner_A-inner_B;    // SUBTRACT
+                0: begin
+                    {inner_cout, inner_result} = inner_A+inner_B;  // ADD
+                    overflow_result = inner_cout != inner_result[31];
+                end
+                1: begin
+                    {inner_cout, inner_result} = inner_A-inner_B;    // SUBTRACT
+                    overflow_result = inner_cout != inner_result[31];
+                end
                 2: inner_result = inner_A & inner_B;            // AND
                 3: inner_result = inner_A | inner_B;            // OR
                 4: inner_result = inner_A << ctrl_shiftamt;        // SLL
