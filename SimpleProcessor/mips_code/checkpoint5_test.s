cf88b9a9c588c7f7955ee68333bef3002d824224
JinchengHeRyan <delteforce@outlook.com>
2022-11-29 17:16:09 -0500

    Add bne, jal, jr instruction tests and passed

--- SimpleProcessor/mips_code/checkpoint5_test.s
+++ SimpleProcessor/mips_code/checkpoint5_test.s
@@ -8,3 +8,26 @@ addi $1, $0, 6      # should not be executed
 
 _jump1:
 add $20, $0, $1     # test $1
+
+# ====================
+# Test bne, jal, jr
+addi $1, $0, 1      # $1 = 1
+addi $2, $0, 4      # $2 = 4
+
+_for_loop_1:
+bne $1, $2, _else_1
+j _out_for_loop_1
+
+
+_else_1:
+jal _func_1
+add $20, $3, $0     # $20 = 2, 4, 6
+addi $1, $1, 1
+j _for_loop_1
+
+_out_for_loop_1:
+halt
+
+_func_1:
+add $3, $1, $1
+jr $ra
